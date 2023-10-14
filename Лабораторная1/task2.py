311aea0a577b6f0d374396519ffb50e04efc1f27
sonix14 <117933964+sonix14@users.noreply.github.com>
2023-10-14 21:02:00 +0300

    Update task2.py

--- Лабораторная1/task2.py
+++ Лабораторная1/task2.py
@@ -1,7 +1,9 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 
 # TODO Разделите участников на две команды
-team1 = list_players[:3]
-team2 = list_players[3:]
+list_count = len(list_players)
+players_count = int(list_count / 2)
+team1 = list_players[:players_count]
+team2 = list_players[players_count:]
 print(team1)
 print(team2)
